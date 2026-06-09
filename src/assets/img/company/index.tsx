cd26c05408b8757bca8487d704032c8d6f417d1a
santohack <santohack238@gmail.com>
2026-06-09 15:09:28 +0530

    leadership in progress

---
+++ src/assets/img/company/index.tsx
@@ -0,0 +1,27 @@
+import Glenn from './leadership/glenn.jpg';
+import AdemHeaps from './leadership/adem-heaps.jpeg';
+import JaneOsborne from './leadership/jane-osborne.jpeg';
+import NashimAhmed from './leadership/nasim-ahmad.png';
+import ANdyRodger from './leadership/andy-rodger.jpg';
+import JosephHoward from './leadership/joseph-howard.png';
+import TracyHaynes from './leadership/tracey-haynes.jpg';
+import BrunaLopes from './leadership/bruna-lopes.png';
+import Victoria from './leadership/victoria.jpg';
+import Santosh from './leadership/Santosh.jpeg';
+import Hamna from './leadership/hamna-jalil.png';
+import Mae from './leadership/Mae-Professional.jpg';
+
+export {
+  Glenn,
+  AdemHeaps,
+  JaneOsborne,
+  NashimAhmed,
+  ANdyRodger,
+  JosephHoward,
+  TracyHaynes,
+  BrunaLopes,
+  Victoria,
+  Santosh,
+  Hamna,
+  Mae,
+};
